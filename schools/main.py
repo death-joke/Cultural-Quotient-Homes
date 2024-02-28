a4f80de1ba367ca6972997d530e3c9618c8d8314
MarreTeint <martinzs1@yahoo.fr>
2024-02-28 19:11:33 +0100

    filter

--- schools/main.py
+++ schools/main.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 def parse_data():
     data = pd.read_csv('data.csv', sep=';')
+    data = data[['appellation_officielle', 'denomination_principale', 'libelle_commune', 'code_postal_uai']]
     return data
 
 print(parse_data())
